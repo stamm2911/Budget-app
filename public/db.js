a47dfbd25dd838b4b42b233386380ec3535898fe
Eduardo Stamm <edstamm29@gmail.com>
2021-07-20 00:22:28 -0500

    db creation

--- public/db.js
+++ public/db.js
@@ -1,1 +1,26 @@
+let db;
+let budgetVersion;
+
+// Create a new db request for a "budget" database.
+const request = indexedDB.open('BudgetDB', budgetVersion || 21);
+
+request.onupgradeneeded = function (e) {
+  console.log('Upgrade needed in IndexDB');
+
+  const { oldVersion } = e;
+  const newVersion = e.newVersion || db.version;
+
+  console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
+
+  db = e.target.result;
+
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore('BudgetStore', { autoIncrement: true });
+  }
+};
+
+request.onerror = function (e) {
+  console.log(`Woops! ${e.target.errorCode}`);
+};
+
 
