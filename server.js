2d7b5bbfc595d9699d476428926d03640ffd3b10
Eduardo Stamm <edstamm29@gmail.com>
2021-07-21 13:10:07 -0500

    deploying4

--- server.js
+++ server.js
@@ -15,10 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb+srv://stamm-admin:wrNn2jjDCX23qoxz@cluster0.luteo.mongodb.net/budget?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
